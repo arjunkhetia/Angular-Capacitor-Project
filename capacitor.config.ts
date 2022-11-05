8710701d1340e72081fb9c6ade559881ba1fdf40
Arjun Khetia <arjunkhetia@gmail.com>
2022-11-05 22:00:40 +0530

    Angular + Capacitor Project

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.arjunkhetia.app',
+  appName: 'Angular-Capacitor-Project',
+  webDir: 'dist/angular-capacitor-project',
+  bundledWebRuntime: false
+};
+
+export default config;
