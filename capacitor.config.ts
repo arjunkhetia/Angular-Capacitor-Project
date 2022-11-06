d783c6ecb534f02599eeb0f23c7aadd43b2c3d19
Arjun Khetia <arjunkhetia@gmail.com>
2022-11-06 11:42:42 +0530

    Angular + Capacitor Project

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,7 +4,11 @@ const config: CapacitorConfig = {
   appId: 'com.arjunkhetia.app',
   appName: 'Angular-Capacitor-Project',
   webDir: 'dist/angular-capacitor-project',
-  bundledWebRuntime: false
+  bundledWebRuntime: false,
+  server: {
+    url: 'http://192.168.1.3:4200',
+    cleartext: true
+  }
 };
 
 export default config;
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Geolocation } from '@capacitor/geolocation';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'Angular-Capacitor-Project';
+  location: any;
+
+  ngOnInit(): void {
+    this.getCurrentPosition();
+  }
+
+  async getCurrentPosition() {
+    this.location = await Geolocation.getCurrentPosition();
+  }
 }
